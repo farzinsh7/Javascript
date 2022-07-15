7dae8d53ba1825689ffd84f83f58ada26ca6ae50
farzinsh7 <farzinnater@gmail.com>
2022-07-15 13:43:09 +0430

    different between var & let

---
+++ MyScript.js
@@ -0,0 +1,40 @@
+// Fun game
+// alert("Who are you???");
+// alert("I said who are you???");
+// alert("What are doing here?");
+// alert("Fuck off");
+// document.write("Look for password");
+// console.log("Here is your password 'Fuck You :D'");
+
+//----------------
+
+// //create my First variable
+// let firstname = "Farzin";
+// let lastname = "Shams";
+// let age = 31;
+
+// let access1 = "Yes";
+// const access2 = "Yes";
+// /*
+// P.S.: Both of them show same result but in "const" you can't rewrite your variable****
+// */
+
+// console.log(firstname, lastname, age)
+
+//-------------------------------
+
+// //Different between "var" and "let"
+// /* you cant create another "let" variable with exact name again
+// but you can do that in var without any problem. look the examples */
+// let mylet = 10;
+// var myvar = 20;
+
+// let mylet = 100; // return syntax error
+// var myvar = 200; // work fine
+
+// console.log(mylet) // return syntax error
+// console.log(myvar) // work fine
+
+// all in all better use "let" in modern javascript
+
+//-------------------------------
